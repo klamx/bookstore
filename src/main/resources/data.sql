5ac7486ca9e2dfd37e6f8bade6f8935313fcd11d
Alejandro Largo <largoalejo@gmail.com>
2023-11-19 16:50:29 -0500

    feature: dependencias para test e import.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into books (author, genre, publication_year, title) values ( 'Javier de Frutos', 'Caer', '2014-09-04', 'Aventura' );
+insert into books (author, genre, publication_year, title) values ( 'Francisco Ortega', 'El verbo Kaifman', '2006-01-01', 'Policial' );
+insert into books (author, genre, publication_year, title) values ( 'Alexander Weinberg', 'La conjura', '1998-06-08', 'Intriga' );
+insert into books (author, genre, publication_year, title) values ( 'Brad Thor', 'El primer mandamiento', '2011-03-04', 'Aventura' );
+insert into books (author, genre, publication_year, title) values ( 'Greg Iles', 'Las huellas de dios', '2011-03-05', 'Intriga' );
